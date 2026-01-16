b89670e0c6c1595868b9771fecb3159e319338c0
Lê Giang <legiang30032004@gmail.com>
2026-01-16 11:27:56 +0700

    feat: Scaffold DB

---
+++ PropertyManagementSystem/PropertyManagementSystem.DAL/Entities/Property.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PropertyManagementSystem.DAL.Entities;
+
+public partial class Property
+{
+    public int Id { get; set; }
+
+    public int LandlordId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string Address { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+
+    public string? District { get; set; }
+
+    public decimal? Latitude { get; set; }
+
+    public decimal? Longitude { get; set; }
+
+    public string PropertyType { get; set; } = null!;
+
+    public int? Bedrooms { get; set; }
+
+    public int? Bathrooms { get; set; }
+
+    public decimal? Area { get; set; }
+
+    public decimal? BaseRentPrice { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual ICollection<ChatRoom> ChatRooms { get; set; } = new List<ChatRoom>();
+
+    public virtual User Landlord { get; set; } = null!;
+
+    public virtual ICollection<MaintenanceRequest> MaintenanceRequests { get; set; } = new List<MaintenanceRequest>();
+
+    public virtual ICollection<PropertyImage> PropertyImages { get; set; } = new List<PropertyImage>();
+
+    public virtual ICollection<RentalApplication> RentalApplications { get; set; } = new List<RentalApplication>();
+
+    public virtual ICollection<RentalContract> RentalContracts { get; set; } = new List<RentalContract>();
+
+    public virtual ICollection<ViewingSchedule> ViewingSchedules { get; set; } = new List<ViewingSchedule>();
+}
---
+++ PropertyManagementSystem/PropertyManagementSystem.DAL/Entities/RentalContract.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PropertyManagementSystem.DAL.Entities;
+
+public partial class RentalContract
+{
+    public int Id { get; set; }
+
+    public int PropertyId { get; set; }
+
+    public int LandlordId { get; set; }
+
+    public int TenantId { get; set; }
+
+    public string? ContractNumber { get; set; }
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public decimal RentAmount { get; set; }
+
+    public decimal? DepositAmount { get; set; }
+
+    public string PaymentCycle { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public DateTime? SignedAtLandlord { get; set; }
+
+    public DateTime? SignedAtTenant { get; set; }
+
+    public DateTime? TerminatedAt { get; set; }
+
+    public string? TerminationReason { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual ICollection<ChatRoom> ChatRooms { get; set; } = new List<ChatRoom>();
+
+    public virtual ICollection<ContractFile> ContractFiles { get; set; } = new List<ContractFile>();
+
+    public virtual User Landlord { get; set; } = null!;
+
+    public virtual ICollection<MaintenanceRequest> MaintenanceRequests { get; set; } = new List<MaintenanceRequest>();
+
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    public virtual Property Property { get; set; } = null!;
+
+    public virtual User Tenant { get; set; } = null!;
+}
